f1bc9c8c3d18cbba59e967063f01d804088c62ab
hamza-yameen <hamzayameen3000gmail.com>
2025-03-21 11:45:49 +0500

    client

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Insurgent - AI App</p>
+        <p>Insurgent - AI App with CICD</p>
       </header>
     </div>
   );
